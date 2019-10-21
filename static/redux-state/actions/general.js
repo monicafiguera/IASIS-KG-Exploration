95e2d12d115985cf5465ca9702c581587faf990c
Monica Figuera <monicafigueray@gmail.com>
2019-10-21 23:02:30 +0200

    unused files removed (for creating Venn Diagram)

--- static/redux-state/actions/general.js
+++
@@ -1,5 +0,0 @@
-export const SET_DATASET = "SET_DATASET";
-
-export function setCurrentDataset(name) {
-    return {type: SET_DATASET, name};
-}
--- static/redux-state/actions/ui.js
+++
@@ -1,5 +0,0 @@
-export const SET_ACTIVE_TAB = "SET_ACTIVE_TAB";
-
-export function changeActiveSidebarTab(index) {
-    return { type: SET_ACTIVE_TAB, index};
-}
