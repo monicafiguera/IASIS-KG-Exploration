a2592dedb57387f4e36fd9645f01439655a71e41
Monica Figuera <monicafigueray@gmail.com>
2019-10-28 00:44:21 +0100

    popping tippy in nodes and edges (testing base behavior)

--- static/js/libs/code.js
+++ static/js/libs/code.js
@@ -1,5 +1,3 @@
-/* global Promise, fetch, window, cytoscape, document, tippy, _ */
-
 Promise.all([
   fetch('/static/js/libs/cy-style.json')
     .then(function(res) {
@@ -11,245 +9,49 @@ Promise.all([
     })
 ])
   .then(function(dataArray) {
-    var h = function(tag, attrs, children){
-      var el = document.createElement(tag);
 
-      Object.keys(attrs).forEach(function(key){
-        var val = attrs[key];
 
-        el.setAttribute(key, val);
-      });
+  var cy = (window.cy = cytoscape({
+    container: document.getElementById("cy"),
+    style: dataArray[0],
+    elements: dataArray[1],
+    layout: {
+      name: "cola"
+    }
+  }));
 
-      children.forEach(function(child){
-        el.appendChild(child);
-      });
+  function makePopper(ele) {
+    let ref = ele.popperRef(); // used only for positioning
 
-      return el;
-    };
+    ele.tippy = tippy(ref, { // tippy options:
+      content: () => {
+        let content = document.createElement('div');
+        if (ele._private.group === 'nodes') {
+            content.innerHTML = "No. patients: " + ele.data('patients');
+        } else {
+            content.innerHTML = "Patients: " + ele.data('patients');
+        }
 
-    var t = function(text){
-      var el = document.createTextNode(text);
-
-      return el;
-    };
-
-    var $ = document.querySelector.bind(document);
-
-    var cy = window.cy = cytoscape({
-      container: document.getElementById('cy'),
-      style: dataArray[0],
-      elements: dataArray[1],
-      layout: { name: 'random' }
-    });
-
-    var params = {
-      name: 'cola',
-      nodeSpacing: 5,
-      edgeLengthVal: 45,
-      animate: true,
-      randomize: false,
-      maxSimulationTime: 1500
-    };
-    var layout = makeLayout();
-
-    layout.run();
-
-    var $btnParam = h('div', {
-      'class': 'param'
-    }, []);
-
-    var $config = $('#config');
-
-    $config.appendChild( $btnParam );
-
-    var sliders = [
-      {
-        label: 'Edge length',
-        param: 'edgeLengthVal',
-        min: 1,
-        max: 200
+        return content;
       },
-
-      {
-        label: 'Node spacing',
-        param: 'nodeSpacing',
-        min: 1,
-        max: 50
-      }
-    ];
-
-    var buttons = [
-      {
-        label: h('span', { 'class': 'fa fa-random' }, []),
-        layoutOpts: {
-          randomize: true,
-          flow: null
-        }
-      },
-
-      {
-        label: h('span', { 'class': 'fa fa-long-arrow-down' }, []),
-        layoutOpts: {
-          flow: { axis: 'y', minSeparation: 30 }
-        }
-      }
-    ];
-
-    sliders.forEach( makeSlider );
-
-    buttons.forEach( makeButton );
-
-    function makeLayout( opts ){
-      params.randomize = false;
-      params.edgeLength = function(e){ return params.edgeLengthVal / e.data('weight'); };
-
-      for( var i in opts ){
-        params[i] = opts[i];
-      }
-
-      return cy.layout( params );
-    }
-
-    function makeSlider( opts ){
-      var $input = h('input', {
-        id: 'slider-'+opts.param,
-        type: 'range',
-        min: opts.min,
-        max: opts.max,
-        step: 1,
-        value: params[ opts.param ],
-        'class': 'slider'
-      }, []);
-
-      var $param = h('div', { 'class': 'param' }, []);
-
-      var $label = h('label', { 'class': 'label label-default', for: 'slider-'+opts.param }, [ t(opts.label) ]);
-
-      $param.appendChild( $label );
-      $param.appendChild( $input );
-
-      $config.appendChild( $param );
-
-      var update = _.throttle(function(){
-        params[ opts.param ] = $input.value;
-
-        layout.stop();
-        layout = makeLayout();
-        layout.run();
-      }, 1000/30);
-
-      $input.addEventListener('input', update);
-      $input.addEventListener('change', update);
-    }
-
-    function makeButton( opts ){
-      var $button = h('button', { 'class': 'btn btn-default' }, [ opts.label ]);
-
-      $btnParam.appendChild( $button );
-
-      $button.addEventListener('click', function(){
-        layout.stop();
-
-        if( opts.fn ){ opts.fn(); }
-
-        layout = makeLayout( opts.layoutOpts );
-        layout.run();
-      });
-    }
-
-    var makeTippy = function(node, html){
-      return tippy( node.popperRef(), {
-        html: html,
-        trigger: 'manual',
-        arrow: true,
-        placement: 'bottom',
-        hideOnClick: false,
-        interactive: true
-      } ).tooltips[0];
-    };
-
-    var hideTippy = function(node){
-      var tippy = node.data('tippy');
-
-      if(tippy != null){
-        tippy.hide();
-      }
-    };
-
-    var hideAllTippies = function(){
-      cy.nodes().forEach(hideTippy);
-      cy.edges().forEach(hideTippy);
-    };
-
-    cy.on('tap', function(e){
-      if(e.target === cy){
-        hideAllTippies();
-      }
+      trigger: 'manual' // probably want manual mode
     });
+  }
 
-    cy.on('tap', 'edge', function(e){
-      hideAllTippies();
+  cy.ready(function() {
+    cy.elements().forEach(function(ele) {
+      makePopper(ele);
     });
-
-    cy.on('zoom pan', function(e){
-      hideAllTippies();
-    });
-
-    cy.edges().forEach(function(e){
-        var g = e.data('id');
-
-        var p = e.data('patients');
-        console.log("e",e, p)
-
-        var $links = [
-        {
-          name: 'Patients:' + p.toString(),
-          url: p
-        }
-        ].map(function( link ){
-        return h('div', { 'class': '' }, [ t(link.name) ]);
-        });
-
-        var tippy = makeTippy(e, h('div', {}, $links));
-
-        e.data('tippy', tippy);
-
-        e.on('click', function(e){
-        tippy.show();
-
-        cy.edges().not(e).forEach(hideTippy);
-        });
-    });
-
-    cy.nodes().forEach(function(n){
-      var g = n.data('name');
-
-      var p = n.data('patients');
-
-      var $links = [
-        {
-          name: 'Patients:' + p.toString(),
-          url: p
-        }
-      ].map(function( link ){
-        return h('a', { target: '_blank', href: link.url, 'class': 'tip-link' }, [ t(link.name) ]);
-      });
-
-      var tippy = makeTippy(n, h('div', {}, $links));
-
-      n.data('tippy', tippy);
-
-      n.on('click', function(e){
-        tippy.show();
-
-        cy.nodes().not(n).forEach(hideTippy);
-      });
-    });
-
-    $('#config-toggle').addEventListener('click', function(){
-      $('body').classList.toggle('config-closed');
-
-      cy.resize();
-    });
-
   });
+
+  cy.elements().unbind('mouseover');
+  cy.elements().bind('mouseover', (event) => {
+      event.target.tippy.show();
+       cy.elements().forEach(function(ele) {
+           if (ele !== event.target) {
+            ele.tippy.hide();
+           }
+       });
+  }) ;
+
+});
